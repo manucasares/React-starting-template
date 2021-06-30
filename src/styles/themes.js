2b9ef9433d47f671ec1846a7871c9fa09328aca8
Manuel Casares <manucasaresrojas@gmail.com>
2021-06-30 19:38:56 -0300

    borrada propiedad en maintheme, eliminado animated css

--- src/styles/themes.js
+++ src/styles/themes.js
@@ -4,27 +4,13 @@ import { orange, green } from "@material-ui/core/colors";
 export const mainTheme = createMuiTheme({
     palette: {
         primary: {
-            main: orange[ 500 ],
-            contrastText: '#fff',
+            main: orange[ 500 ]
         },
         secondary: {
-            main: green[ 500 ],
-            contrastText: '#fff',
+            main: green[ 500 ]
         },
-        light: {
-            main: "#fff"
-        }
     },
     typography: {
         fontFamily: 'Open Sans, Arial',
-        /*  Valores aceptados por MUI:
-            h1-h2-h3-h4-h5-h6-subtitle1-subtitle2-body1-body2-button-caption-overline
-        */
-        h3: {
-            // Fluid font sizes
-            fontSize: 'clamp(5px, 3vw, 20px)',
-            // Acá se pueden agregar todos los estilos que queramos
-            color: '#ff0000',
-        }
     },
 });
