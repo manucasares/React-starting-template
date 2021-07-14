ab498b4a8a99360d27cc00ef177fa4cfed21ee84
Manuel Casares Rojas <manucasaresrojas@gmail.com>
2021-07-14 14:48:22 -0300

    Update themes.js

--- src/styles/themes.js
+++ src/styles/themes.js
@@ -4,6 +4,17 @@ export const theme = {
     colors: {
         main: '#333',
     },
+    fz: {
+        '200': 'clamp(12px, 2.5vw, 20px)',
+        '300': 'clamp(15px, 2.8vw, 24px)',
+        '400': 'clamp(18px, 3.2vw, 28px)',
+        '500': 'clamp(20px, 3.6vw, 32px)',
+        '600': 'clamp(30px, 4.8vw, 50px)',
+        '700': 'clamp(38px, 6vw, 65px)',
+        '800': 'clamp(50px, 8vw, 80px)',
+        '900': 'clamp(65px, 10vw, 100px)',
+        '1000': 'clamp(70px, 13vw, 120px)',
+    },
     mediaQueries: {
         above768: '(min-width: 768px)'
     }
